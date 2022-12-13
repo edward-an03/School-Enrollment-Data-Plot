4c2174a235b217194706420b5a8b4bd82a06b7a8
Edward An <117535243+edward-an03@users.noreply.github.com>
2022-12-12 18:55:02 -0700

    Update school_enrollment_data_plot.py

--- school_enrollment_data_plot.py
+++ school_enrollment_data_plot.py
@@ -33,7 +33,7 @@ school_data_2018_2019 = np.genfromtxt("SchoolData_2018-2019.csv", delimiter = ",
 school_data_2019_2020 = np.genfromtxt("SchoolData_2019-2020.csv", delimiter = ",", skip_header = True)
 school_data_2020_2021 = np.genfromtxt("SchoolData_2020-2021.csv", delimiter = ",", skip_header = True)
 
-# Hint: Create a dictionary for all school names and codes
+# Dictionary with school codes and school names
 school_dictionary = {"1224":"Centennial High School", "1679":"Robert Thirsk School", "9626":"Louise Dean School",\
                 "9806":"Queen Elizabeth High School", "9813":"Forest Lawn High School", "9815":"Crescent Heights High School",\
                 "9816":"Western Canada High School", "9823":"Central Memorial High School", "9825":"James Fowler High School",\
@@ -42,7 +42,7 @@ school_dictionary = {"1224":"Centennial High School", "1679":"Robert Thirsk Scho
                 "9856":"Jack James High School", "9857":"Sir Winston Churchill High School", "9858":"Dr. E. P. Scarlett High School",\
                 "9860":"John G Diefenbaker High School" ,"9865":"Lester B. Pearson High School"}
 
-# Hint: Create a list of school codes to help with index look-up in arrays
+# List of school codes and school names dervied from school_dictionary. Created for the purpose of index finding as dictionaries don't have ordered indices
 school_codes_list = list(school_dictionary.keys()) 
 school_names_list = list(school_dictionary.values())
 
@@ -53,12 +53,12 @@ def calculate_mean_enrollment(school_code, grade):
         Parameters: school_code (str), grade (int)
         Return: enrollment_mean (int)
     """
-    row_index = school_codes_list.index(school_code) #school is determined by this index
-    column_index = grade - 9 #grade level is determined by this index
-    enrollment_2018_2019 = school_data_2018_2019[row_index][column_index] #gets enrollment values for all three years
+    row_index = school_codes_list.index(school_code) #s chool is determined by this index
+    column_index = grade - 9 # grade level is determined by this index
+    enrollment_2018_2019 = school_data_2018_2019[row_index][column_index] # gets enrollment values for all three years
     enrollment_2019_2020 = school_data_2019_2020[row_index][column_index]
     enrollment_2020_2021 = school_data_2020_2021[row_index][column_index]
-    enrollment_mean = math.floor((enrollment_2018_2019 + enrollment_2019_2020 + enrollment_2020_2021)/3) #average calculation
+    enrollment_mean = math.floor((enrollment_2018_2019 + enrollment_2019_2020 + enrollment_2020_2021)/3) # average calculation
     return enrollment_mean
 
 def alumni_2019_2021(school_code):
@@ -68,8 +68,8 @@ def alumni_2019_2021(school_code):
         Parameters: school_code(str)
         Return: total_alumni (int)
     """
-    index = school_codes_list.index(school_code) #school is determined by this index
-    alumni_2019 = school_data_2018_2019[index][3] #gets number of grade 12 students for all three years
+    index = school_codes_list.index(school_code) # school is determined by this index
+    alumni_2019 = school_data_2018_2019[index][3] # gets number of grade 12 students for all three years
     alumni_2020 = school_data_2019_2020[index][3]
     alumni_2021 = school_data_2020_2021[index][3]
     total_alumni = math.floor(alumni_2019 + alumni_2020 + alumni_2021) #finds the total
@@ -82,9 +82,9 @@ def create_y_values_figure_1(school_code, year):
         Parameters: school_code(str), year (num)
         Return: y_values (list)
     """
-    index = school_codes_list.index(school_code) #school is determined by this index
+    index = school_codes_list.index(school_code) # school is determined by this index
     y_values = []
-    if year == 2019: #depending on the year passed as the argument, the file specific to that year is used
+    if year == 2019: # depending on the year passed as the argument, the file specific to that year is used
         y_values.append(school_data_2018_2019[index][1])
         y_values.append(school_data_2018_2019[index][2])
         y_values.append(school_data_2018_2019[index][3])
@@ -105,19 +105,18 @@ def create_y_values_figure_2(school_code, grade):
         Parameters: school_code(str), grade (num)
         Return: y_values (list)
     """
-    row_index = school_codes_list.index(school_code) #school is determined by this index
-    column_index = grade - 9 #grade level is determined by this index
+    row_index = school_codes_list.index(school_code) # school is determined by this index
+    column_index = grade - 9 # grade level is determined by this index
     y_values = []
     y_values.append(school_data_2018_2019[row_index][column_index])
     y_values.append(school_data_2019_2020[row_index][column_index])
     y_values.append(school_data_2020_2021[row_index][column_index])
     return y_values
 
-# Add your code within the main function. A docstring is not required for this function.
 def main(): 
     print("Welcome to School Enrollment Statistics\n")
 
-    # Print array data here
+    # Array data print
     print("Array data for 2020-2021:")
     print(school_data_2020_2021)
     print()
@@ -157,7 +156,7 @@ def main():
     school1 = School(school_name, school_code)
     school1.print_all_stats() #printing school name and school code
 
-    # Add data processing and plotting here
+    # Data processing
     grade10_enrollment_mean = calculate_mean_enrollment(school_code, 10)
     grade11_enrollment_mean = calculate_mean_enrollment(school_code, 11)
     grade12_enrollment_mean = calculate_mean_enrollment(school_code, 12)
